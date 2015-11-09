d690b4628b4bc62bf58fe8fe9b8e047bd2e79afa
despairfactor <matthewdalex92@gmail.com>
2015-11-09 12:21:36 -0500

    target: product: implement filter for libdrmclearkeyplugin

--- target/product/base.mk
+++ target/product/base.mk
@@ -53,7 +53,6 @@ PRODUCT_PACKAGES += \
     libcameraservice \
     libcamera2ndk \
     libdl \
-    libdrmclearkeyplugin \
     libeffectproxy \
     libeffects \
     libinput \
@@ -128,6 +127,11 @@ PRODUCT_PACKAGES += \
     vdc \
     vold \
     wm
+
+ifneq ($(filter nougat_shamu,$(TARGET_PRODUCT)),)
+PRODUCT_PACKAGES += \
+    libdrmclearkeyplugin 
+endif
 
 
 PRODUCT_COPY_FILES := $(call add-to-product-copy-files-if-exists,\
