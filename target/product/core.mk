5fc4e2e2deea7322ef0965919f76af0c530a59a1
Chet Kener <Cl3Kener@gmail.com>
2016-08-27 10:04:32 -0400

    Kill Bloat with Fire!
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -21,11 +21,8 @@
 # core_tiny.mk in addition to core_minimal.mk.
 
 PRODUCT_PACKAGES += \
-    BasicDreams \
     BlockedNumberProvider \
-    BookmarkProvider \
-    Browser2 \
-    Calendar \
+    Calculator \
     CalendarProvider \
     CaptivePortalLogin \
     CertInstaller \
@@ -33,7 +30,6 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
     ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
@@ -41,14 +37,11 @@ PRODUCT_PACKAGES += \
     KeyChain \
     Keyguard \
     LatinIME \
-    Launcher2 \
+    Launcher3 \
     ManagedProvisioning \
-    MtpDocumentsProvider \
-    PicoTts \
     PacProcessor \
     libpac \
     PrintSpooler \
-    PrintRecommendationService \
     ProxyHandler \
     QuickSearchBox \
     Settings \
